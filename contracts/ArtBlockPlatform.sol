e889dfd8849ac7eb4fd03f072980bae8bbab205c
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-11-03 17:05:35 +0600

    Implement ERC721 Artwork NFT

--- contracts/ArtBlockPlatform.sol
+++ contracts/ArtBlockPlatform.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract ABXToken is ERC20, ERC20Permit, Ownable {
     constructor() ERC20("ArtBlockToken", "ABX") ERC20Permit("ArtBlockToken") Ownable() {
@@ -38,6 +43,28 @@ contract ArtBlockPlatform is Ownable {
 
     function withdrawEther() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
+    }
+}
+
+contract ArtNFT is ERC721, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("ArtNFT", "ANFT") Ownable() {}
+
+    function safeMint(address to) external onlyOwner returns (uint256) {
+        uint256 tokenId = ++_nextTokenId;
+        _safeMint(to, tokenId);
+        return tokenId;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
 
