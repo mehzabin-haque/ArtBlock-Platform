8a083f83f016c7a9a5e0dc83226a4d387b97707b
shazzad5709 <shazzad1111@gmail.com>
2023-11-04 01:47:14 +0600

    Add buy abx function

--- frontend/src/pages/buy.tsx
+++ frontend/src/pages/buy.tsx
@@ -1,72 +1,62 @@
-import React, { useState } from 'react'
+'use client'
+
+import { BaseError, parseEther } from 'viem'
 import { useContractWrite, useWaitForTransaction, usePrepareContractWrite } from 'wagmi'
-import { ArtBlockPlatform__factory, Greeter__factory } from '../../typechain'
+import { ArtBlockPlatform__factory } from '../../typechain'
+import { stringify } from '../utils/stringify'
+
 import contractDetails from '../info/contractDetails.json'
-import { IoArrowBackCircleSharp } from 'react-icons/io5'
-import Link from 'next/link'
-import Converter from '../components/Convert'
-import { parseEther } from 'viem'
-import ConnectWallet from 'components/Connect/ConnectWallet'
-import { utils } from 'ethers'
+import { useState } from 'react'
 
-type Props = {}
-const Exchange = (props: Props) => {
-
-  const [currentValueEther, setCurrentValueEther] = useState('')
-  const [currentValueABX, setCurrentValueABX] = useState('')
+export function Buy() {
+  const [currentValue, setCurrentValue] = useState('');
 
   const { config } = usePrepareContractWrite({
-    address: contractDetails.ABXTokencontractAddress as `0x${string}`,
+    address: contractDetails.ABXContact as 0x${string},
     abi: ArtBlockPlatform__factory.abi,
     functionName: 'purchaseABX',
-    args: [],
-    value: parseEther(currentValueEther)
+    value: parseEther("0.1")
   })
-
   const { data, isLoading, isSuccess, write } = useContractWrite(config)
 
   const { data: receipt, isLoading: isPending } = useWaitForTransaction({ hash: data?.hash })
   
-  async function  handleSubmit(e) {
-    e.preventDefault();
-    console.log('submit')
-    // write()
-  }
-
   function handleChange(evt) {
     console.log(evt.currentTarget.value)
-    setCurrentValueEther(evt.currentTarget.value)
+    setCurrentValue(evt.currentTarget.value)
   }
-
   return (
     <>
-      <Link href='/milestone1'>
-        <IoArrowBackCircleSharp className="h-10 w-10 text-black" aria-hidden="true" />
-      </Link>
-      {/* <div className='flex items-center justify-center'> */}
-      <div>
-        <h4 className="text-center text-sm font-medium">demo: ConnectWalletBtn Full</h4>
-        <div className="flex w-full flex-col items-center">
-          <ConnectWallet />
-        </div>
-      </div>      <Converter />
-      <div className='flex items-center justify-center'>
-        
-          <button
-            onClick={write}
-            onClickCapture={handleSubmit}
-            className="rounded-lg flex items-center justify-center border-yellow-500 bg-yellow-400 p-4 px-8 font-bold uppercase text-gray-800"
-          >
-            Buy ABX
-          </button>
-        {isLoading && (
-          <p className="text-green-500">Success! {currentValueABX} {' '} ABX Token bought</p>
-        )}
-
-      </div>
-      {/* </div> */}
+      <form
+        className="m-4 flex"
+        onSubmit={e => {
+          e.preventDefault()
+          write()
+        }}
+      >
+        <input
+          value={currentValue}
+          onChange={evt => handleChange(evt)}
+          className="mr-0 rounded-l-lg border-b border-l border-t border-gray-200 bg-white p-4 text-gray-800"
+          placeholder="Enter amount"
+        />
+        <button
+          type="submit"
+          className="rounded-r-lg border-b border-r  border-t border-yellow-500 bg-yellow-400 p-4 px-8 font-bold uppercase text-gray-800"
+        >
+          Buy Tx
+        </button>
+      </form>
+      {isLoading && <div>Check wallet...</div>}
+      {isPending && <div>Transaction pending...</div>}
+      {isSuccess && (
+        <>
+          <div>Transaction Hash: {data?.hash}</div>
+          <div>
+            Transaction Link: <a target="_blank" href={"https://sepolia.etherscan.io/tx/"+data?.hash}>Link</a>
+          </div>
+        </>
+      )}
     </>
   )
 }
-
-export default Exchange
