595979cdb76ac20a9d0e0c25e2001736a40d20a4
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-11-03 17:09:11 +0600

    Implement art proposing and voting

--- contracts/ArtBlockPlatform.sol
+++ contracts/ArtBlockPlatform.sol
@@ -83,6 +83,156 @@ contract ArtBlockPlatform is Ownable {
     }
 }
 
+contract Community {
+    address public creator;
+    string public title;
+    string public token;
+    string public syntax;
+    string public description;
+    IERC20 public immutable abxToken;
+    CommunityToken public immutable comToken;
+    ArtNFT public artNFT;
+    address public immutable dex;
+    IExclusiveArt public exclusiveNTT;
+
+    constructor (
+        address _creator,
+        string memory _title,
+        string memory _token,
+        string memory _syntax,
+        string memory _description,
+        address _abxToken,
+        address _exclusiveNTT
+    ) {
+        creator = _creator;
+        title = _title;
+        token = _token;
+        description = _description;
+        comToken = new CommunityToken(_token, _syntax);
+        abxToken = IERC20(_abxToken);
+        dex = initiateDEX();
+        artNFT = new ArtNFT();
+        exclusiveNTT = IExclusiveArt(_exclusiveNTT);
+    }
+
+    function initiateDEX() internal returns (address dexAddress) {
+        require(dex == address(0), "DEX already initialized!");
+
+        DEX _dex = new DEX(address(abxToken), address(this));
+        dexAddress = address(_dex);
+    }
+
+    // ---------------------------
+    // Artwork Minting and Auction creation and bidding
+    struct Artwork {
+        string name;
+        uint price;
+        address artist;
+        uint nftTokenId;
+    }
+
+    Artwork[] public artworks;
+    mapping(uint256 => Artwork) public artInfo;
+
+    event ArtVoted(uint256 proposalId, address voter, int256 vote);
+    event ArtMinted(uint256 proposalId, uint256 tokenId);
+
+    function mintArt(uint proposalId, string memory _name, uint _price) public {
+        require(artProposals[proposalId].winner == true, "Not approved");
+        Artwork storage artwork = artworks.push();
+        artwork.name = _name;
+        artwork.price = _price;
+        artwork.artist = msg.sender;
+        uint256 tokenId = artNFT.safeMint(msg.sender);
+        artInfo[tokenId] = artwork;
+    }
+
+    function setArtist(uint256 _tokenId) public {
+        require(
+            artInfo[_tokenId].artist == address(0),
+            "Artist is already set"
+        );
+        artInfo[_tokenId].artist = msg.sender;
+    }
+
+    // ---------------------------------------------------------------------------------
+    // ArtProposal
+    struct ArtProposal {
+        address artist;
+        uint upvotes;
+        uint downvotes;
+        uint closingTime;
+        bool isMinted;
+        bool threshold;
+        string name;
+        bool isExclusive;
+        bool winner;
+    }
+
+    mapping (uint => ArtProposal) public artProposals;
+    uint public proposalCounter;
+
+    event ArtProposed(uint256 proposalId, string name, address owner);
+    // Voting period = 10 mins
+    // Upvote and Downvote threshold = 3
+    // Stake to propose art = 100 Community Tokens
+
+    function proposeArt(string memory _name, bool _isExclusive) public {
+        require(comToken.transferFrom(msg.sender, address(this), 100), "Transfer failed");
+        artProposals[proposalCounter] = ArtProposal({
+            artist: msg.sender,
+            upvotes: 0,
+            downvotes: 0,
+            closingTime: block.timestamp + 600,
+            isMinted: false,
+            threshold: false,
+            name: _name,
+            isExclusive: _isExclusive,
+            winner: false
+        });
+
+        emit ArtProposed(proposalCounter, _name, msg.sender);
+        proposalCounter++;
+    }
+
+    function upvote(uint proposalId) external {
+        ArtProposal storage _artProposal = artProposals[proposalId];
+        require(_artProposal.closingTime < block.timestamp, "Voting closed");
+        
+        uint weight = (1 ether * comToken.balanceOf(msg.sender) / comToken.totalSupply());
+        _artProposal.upvotes += weight;
+    }
+
+    function downvote(uint proposalId) external {
+        ArtProposal storage _artProposal = artProposals[proposalId];
+        require(_artProposal.closingTime < block.timestamp, "Voting closed");
+        
+        uint weight = (1 ether * comToken.balanceOf(msg.sender) / comToken.totalSupply());
+        _artProposal.downvotes += weight;
+    }
+
+    function generateVotingResult(uint proposalId) external returns (bool) {
+        ArtProposal storage _artProposal = artProposals[proposalId];
+
+        require(
+            _artProposal.closingTime > block.timestamp,
+            "Voting not closed!"
+        );
+
+        if (_artProposal.upvotes > _artProposal.downvotes) {
+            require(_artProposal.isMinted == false, "Art is already minted");
+            require(comToken.transfer(_artProposal.artist, 100), "Transfer failed");
+            _artProposal.winner = true; 
+            return true;
+        }
+        else {
+            return false;
+        } 
+    }
+
+    
+}
+
 interface IExclusiveArt is IERC721 {
     function safeMint(address to) external returns (uint);
 }
